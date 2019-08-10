7597b1aaf9a738e5f84b732f7760c90ab4ce8e91
IBMOI <hexwhyzet@gmail.com>
2019-08-10 13:58:40 +0300

    Added settings saving

--- Android/app/src/main/java/com/example/procrastinationgame/MainActivity.kt
+++ Android/app/src/main/java/com/example/procrastinationgame/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.example.procrastinationgame
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.SharedPreferences
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
+
+import android.os.Handler
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.view.View
+import android.view.Window
+import android.support.v4.os.HandlerCompat.postDelayed
 
 
 class MainActivity : AppCompatActivity() {
@@ -42,6 +53,24 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
             }
         }
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "example1"
+            val descriptionText = "example2"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("nc1", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onPause() {
@@ -52,6 +81,36 @@ class MainActivity : AppCompatActivity() {
         preferencesEditor.putBoolean(IS_DEBUG_MODE, isDebugMode)
         preferencesEditor.apply()
     }
+
+    fun notify(view: View) {
+        var builder = NotificationCompat.Builder(this, "nc1")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Stop starring at the Screen")
+            .setContentText("You stare at screen too long")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("BUAH!!!")
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(64, builder.build())
+        }
+
+    }
+
+    fun luup(view: View) {
+        val handler = Handler()
+        val delay = 10000 //milliseconds
+
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                notify(view)
+                handler.postDelayed(this, delay.toLong())
+            }
+        }, delay.toLong())
+    }
+
 
 }
 
